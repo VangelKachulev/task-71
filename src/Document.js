762a1304143ad2ca5f693757bac54335470516ac
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-14 14:27:39 +0200

    add text

---
+++ src/Document.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+
+
+export default function Document({ title, content }) {
+
+
+    const handleScroll = (e) => {
+        let element = e.target
+        if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+            document.querySelector('button').disabled = false
+        }
+    }
+
+    return (
+        <div>
+            <h1 className="title">{title}</h1>
+            <p onScroll={handleScroll} style={{ height: "200px", overflow: 'auto' }} className="content">{content}</p>
+            <button disabled={true}>I Agree</button>
+        </div>
+
+
+    )
+}
